67195d2353b19cefa49e4adc8d507d7b42260f83
Cibani <151187399+Cibani@users.noreply.github.com>
2025-10-01 12:33:36 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Smooth scrolling for navigation links
+document.querySelectorAll('nav a').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    document.querySelector(link.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
+  });
+});
